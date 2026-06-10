744eff1908d22df6d6ddf986c3b5e1b5a23295e6
Systeemiisand <146324763+systeemiisand@users.noreply.github.com>
2026-06-10 18:12:13 +0300

    Fix: time sync

--- components/TournamentCountdown.tsx
+++ components/TournamentCountdown.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 export default function TournamentCountdown() {
-  const tournamentStart = new Date("2026-06-11T19:00:00+03:00").getTime();
+  const tournamentStart = new Date("2026-06-10T18:09+03:00").getTime();
 
   const [now, setNow] = useState<number | null>(null);
 
